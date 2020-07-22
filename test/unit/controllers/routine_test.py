89ff2e3eec8ce03877f74ad593fa846a79cf36de
vitorDaynno <vitordaynno@gmail.com>
2020-07-22 00:02:35 -0300

    Melhorias nos testes

--- test/unit/controllers/routine_test.py
+++ test/unit/controllers/routine_test.py
@@ -12,27 +12,51 @@ class RoutineControllerTest(unittest.TestCase):
         try:
             redis_helper = RedisHelper()
             routineController = RoutineController(None, redis_helper)
+            set_value_mock = MagicMock()
+            redis_helper.set_value = set_value_mock
             routineController.set_name(None)
             self.fail()
         except Exception as error:
             self.assertEqual(error.args[0], "Name is required")
+            set_value_mock.assert_not_called()
 
     def test_set_name_with_empty_name(self):
         try:
             redis_helper = RedisHelper()
             routineController = RoutineController(None, redis_helper)
+            set_value_mock = MagicMock()
+            redis_helper.set_value = set_value_mock
             routineController.set_name("")
             self.fail()
         except Exception as error:
             self.assertEqual(error.args[0], "Name should not be empty")
+            set_value_mock.assert_not_called()
+
+    def test_set_name_with_name_but_failed(self):
+        redis_helper = RedisHelper()
+        routineController = RoutineController(1, redis_helper)
+
+        set_value_mock = MagicMock(side_effect=Exception('Error'))
+        redis_helper.set_value = set_value_mock
+
+        try:
+            routineController.set_name("Test")
+            self.fail()
+        except Exception as error:
+            self.assertEqual(error.args[0], "Error")
+            set_value_mock.assert_called_with(
+                "createRoutine§1", '{"name": "Test"}')
 
     def test_set_name_with_name(self):
         redis_helper = RedisHelper()
-        routineController = RoutineController(None, redis_helper)
+        routineController = RoutineController(1, redis_helper)
 
-        magicMock = MagicMock(return_value=True)
-        redis_helper.set_value = magicMock
+        set_value_mock = MagicMock(return_value=True)
+        redis_helper.set_value = set_value_mock
 
         r = routineController.set_name("Test")
 
         self.assertEqual(r, "Test set successfully")
+        set_value_mock.assert_called_once()
+        set_value_mock.assert_called_with(
+            "createRoutine§1", '{"name": "Test"}')
